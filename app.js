b2c2e0baf559c484cffe2bef0d5667f9b1de0a2c
gabriel <gabrieldario1235@gmail.com>
2023-11-15 20:00:55 -0300

    nueva version

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+const electronicos = document.getElementById("electronicos")
+const amazon = document.getElementById("amazon")
+const jueguetes = document.getElementById("jueguetes")
+const hogar = document.getElementById("hogar")
+const seleccionados = document.getElementById("seleccionados")
+const salud = document.getElementById("salud")
+const padre = document.getElementById("padre")
+
+console.log("padre")
+
+
